db8dba6c3718a90a13fe3a9704df6b4428be62a9
JohMast <44399453+JohMast@users.noreply.github.com>
2020-12-10 16:51:20 +0100

    Fixed violins

--- R/ts_makeframes.R
+++ R/ts_makeframes.R
@@ -91,7 +91,7 @@ ts_makeframes <- function(x_list,r_type = NULL,minq = 0.02,maxq = 0.98,samplesiz
     #})
   }else{
     if(is.null(alpha)){
-      alpha=0.5
+      alpha=1
     }
     #make the plots
     r_ggplots <- .ts_makeframes(x_list = r_list_out_stretched,r_type = r_type,gglayer=F,alpha=alpha)
